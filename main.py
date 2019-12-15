b11bef8ccfadaf155662651b43c4cd53f69c80a7
Heman Gandhi <hemangandhi@gmail.com>
2019-12-14 21:09:24 -0500

    cleaner code

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
-NEIGHBORHOOD_DIFFERENCE_THRES = 0.5
+NEIGHBORHOOD_DIFFERENCE_THRES = 128 * (3 ** 0.5)
 
-def distance(c1, c2 = (0, 0, 0)):
+def distance(c1, c2=(0, 0, 0)):
     return sum((x - y) ** 2 for x, y in zip(c1, c2)) ** 0.5
 
 def points_within_distance(x, y, width, height, neighborhood, values):
@@ -15,7 +15,8 @@ def get_border_pixels(width, height, grayscale, neighborhood):
     border_pix = []
     for x in range(width):
         for y in range(height):
-            pts = points_within_distance(x, y, width, height, neighborhood, lambda i, j: grayscale[i][j])
+            pts = points_within_distance(x, y, width, height,
+                                         neighborhood, lambda i, j: grayscale[i][j])
             if sum(pts) >= NEIGHBORHOOD_DIFFERENCE_THRES * len(pts) / 2:
                 border_pix.append((x, y))
     return border_pix
@@ -25,11 +26,14 @@ def main(in_file, out_file, neighborhood):
     pix = im.load()
     width, height = im.size
     grayscale = [[sum(distance(pix[x, y], nbr)
-        for nbr in points_within_distance(x, y, width, height, neighborhood, lambda i, j: pix[i, j]))
-        for y in range(height)]
-        for x in range(width)]
-
-
+                      for nbr in points_within_distance(x, y, width, height,
+                                                        neighborhood, lambda i, j: pix[i, j]))
+                  for y in range(height)]
+                 for x in range(width)]
+    borders = get_border_pixels(width, height, grayscale, neighborhood)
+    with open(out_file, 'w') as output:
+        for b in borders:
+            output.write(str(b))
 
 if __name__ == "__main__":
     from sys import argv
